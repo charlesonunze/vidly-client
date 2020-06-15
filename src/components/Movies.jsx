3d6b4d450d3f7016704793a84fc126790962b5b7
charlesonunze <charlesonunze2@gmail.com>
2020-06-15 11:10:03 +0100

    feat: 🎸 conditional rendering of movies

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -13,38 +13,48 @@ class Movies extends Component {
 	};
 
 	render() {
-		return (
-			<table className='table'>
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Genre</th>
-						<th>Stock</th>
-						<th>Rate</th>
-						<th></th>
-					</tr>
-				</thead>
-
-				<tbody>
-					{this.state.movies.map((movie, index) => (
-						<tr key={index}>
-							<td>{movie.title}</td>
-							<td>{movie.genre.name}</td>
-							<td>{movie.numberInStock}</td>
-							<td>${movie.dailyRentalRate}</td>
-							<td>
-								<button
-									className='btn btn-sm btn-danger'
-									onClick={() => this.deleteMovieHandler(movie._id)}
-								>
-									Delete
-								</button>
-							</td>
+		let movies =
+			!this.state.movies || this.state.movies.length < 1 ? (
+				<h6>
+					There are no movies in the database{' '}
+					<span role='img' aria-label='sad-emoji'>
+						😞
+					</span>
+				</h6>
+			) : (
+				<table className='table'>
+					<thead>
+						<tr>
+							<th>Title</th>
+							<th>Genre</th>
+							<th>Stock</th>
+							<th>Rate</th>
+							<th></th>
 						</tr>
-					))}
-				</tbody>
-			</table>
-		);
+					</thead>
+
+					<tbody>
+						{this.state.movies.map((movie, index) => (
+							<tr key={index}>
+								<td>{movie.title}</td>
+								<td>{movie.genre.name}</td>
+								<td>{movie.numberInStock}</td>
+								<td>${movie.dailyRentalRate}</td>
+								<td>
+									<button
+										className='btn btn-sm btn-danger'
+										onClick={() => this.deleteMovieHandler(movie._id)}
+									>
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			);
+
+		return movies;
 	}
 }
 

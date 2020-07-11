8ddff057f05255d22be0c7fadfc5f640b91cdf2e
charlesonunze <charlesonunze2@gmail.com>
2020-07-11 04:49:13 +0100

    feat: 🎸 decode the current user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import jwtDecode from 'jwt-decode'
 import Movies from './components/Movies';
 import NavBar from './components/NavBar';
 import Customers from './components/Customers';
@@ -9,25 +10,38 @@ import MoviesForm from './components/MoviesForm';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 
-function App() {
-	return (
-		<>
-			<NavBar />
-			<main className='container'>
-				<Switch>
-					<Route path='/register' component={ RegisterForm } />
-					<Route path='/login' component={ LoginForm } />
-					<Route path='/movies/:id' component={ MoviesForm } />
-					<Route path='/movies' component={ Movies } />
-					<Route path='/rentals' component={ Rentals } />
-					<Route path='/customers' component={ Customers } />
-					<Route path='/not-found' component={ NotFound } />
-					<Redirect from='/' exact to='/movies' />
-					<Redirect to='/not-found' />
-				</Switch>
-			</main>
-		</>
-	);
+class App extends Component {
+	state = {}
+
+	componentDidMount() {
+		try {
+			const token = localStorage.getItem('token');
+			const user = jwtDecode(token);
+			this.setState({ user });
+		} catch (error) { }
+	}
+
+	render() {
+		return (
+			<>
+				<NavBar user={ this.state.user } />
+
+				<main className='container'>
+					<Switch>
+						<Route path='/register' component={ RegisterForm } />
+						<Route path='/login' component={ LoginForm } />
+						<Route path='/movies/:id' component={ MoviesForm } />
+						<Route path='/movies' component={ Movies } />
+						<Route path='/rentals' component={ Rentals } />
+						<Route path='/customers' component={ Customers } />
+						<Route path='/not-found' component={ NotFound } />
+						<Redirect from='/' exact to='/movies' />
+						<Redirect to='/not-found' />
+					</Switch>
+				</main>
+			</>
+		);
+	}
 }
 
 export default App;
